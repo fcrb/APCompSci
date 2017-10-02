d9e8f56ab3864af9cb1b493f0d290d87cba81b79
fcrb <freyadiy@gmail.com>
2017-10-01 22:31:40 -0400

    Donzo!

--- Unit1Benson/CircleCircumscribed.java
+++ Unit1Benson/CircleCircumscribed.java
@@ -8,6 +8,15 @@ import java.util.Scanner;
 public class CircleCircumscribed
 {
    public static void main(String [] args){
-       
+               Scanner keyboard = new Scanner(System.in); 
+               prn("What is the length of a?");
+               int a = keyboard.nextInt();
+               prn("What is the length of b?");
+               int b = keyboard.nextInt();
+               double c = Math.sqrt(a * a + b * b);
+               prn("Circumference is " + (c * Math.PI));
+    }
+        static void prn(String q) {
+        System.out.println(q);
     }
 }
--- Unit1Benson/GallonsOfGas.java
+++ Unit1Benson/GallonsOfGas.java
@@ -8,6 +8,27 @@ import java.util.Scanner;
 public class GallonsOfGas
 { 
     public static void main(String [] args){
-        
+        cls();
+        Scanner keyboard = new Scanner(System.in); 
+        prn("How much gas did you use in gallons?");
+        double gallons = keyboard.nextDouble();
+        prn("What's the price per gallon?");
+        double price = keyboard.nextDouble();
+        prn("Odometer reading before the trip in miles?");
+        int beforeOdom = keyboard.nextInt();
+        prn("Odometer reading after the trip in miles?");
+        int afterOdom = keyboard.nextInt();
+        System.out.format("%.2f mpg\n", (afterOdom - beforeOdom) / gallons);
+        System.out.format("$%.2f spent", gallons * price);
+    }
+
+    static void prn(String q) {
+        System.out.println(q);
+    }
+
+    static void cls() {
+        for(int i = 0; i < 50; ++i) {
+            System.out.println("");
+        }
     }
 }
